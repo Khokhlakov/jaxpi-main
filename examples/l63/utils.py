fe7a492909499e03701e905b25c3576da1801cad
Khokhlakov <jmartinezcas@unal.edu.co>
2026-03-05 11:12:11 -0500

    Fixed data/l63_t5.mat path

--- examples/l63/utils.py
+++ examples/l63/utils.py
@@ -2,7 +2,7 @@ import scipy.io
 
 
 def get_dataset():
-    data = scipy.io.loadmat("data/l63_t5.mat")
+    data = scipy.io.loadmat("data/l63.mat")
     xyz_ref = data["usol"]
     t_star = data["t"].flatten()
 
