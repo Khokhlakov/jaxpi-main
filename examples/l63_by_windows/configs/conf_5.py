e959f00e1ea9d20e750fa7313496847425e877a9
Khokhlakov <jmartinezcas@unal.edu.co>
2026-03-08 00:05:28 -0500

    update config 5

--- examples/l63_by_windows/configs/conf_5.py
+++ examples/l63_by_windows/configs/conf_5.py
@@ -16,14 +16,14 @@ def get_config():
     # Arch (Keeping L63 specific architecture)
     config.arch = arch = ml_collections.ConfigDict()
     arch.arch_name = "Mlp"
-    arch.num_layers = 6
-    arch.hidden_dim = 256
+    arch.num_layers = 4
+    arch.hidden_dim = 512
     arch.out_dim = 3
     arch.activation = "tanh"
     arch.periodicity = ml_collections.ConfigDict(
         {"period": (jnp.pi,), "axis": (1,), "trainable": (False,)}
     )
-    arch.fourier_emb = ml_collections.ConfigDict({"embed_scale": 5, "embed_dim": 256})
+    arch.fourier_emb = ml_collections.ConfigDict({"embed_scale": 1, "embed_dim": 256})
     arch.reparam = ml_collections.ConfigDict(
         {"type": "weight_fact", "mean": 0.5, "stddev": 0.1}
     )
@@ -43,7 +43,7 @@ def get_config():
     config.training = training = ml_collections.ConfigDict()
     training.max_steps = 250000 
     training.batch_size_per_device = 4096
-    training.num_time_windows = 10 
+    training.num_time_windows = 12
 
     # Weighting
     config.weighting = weighting = ml_collections.ConfigDict()
