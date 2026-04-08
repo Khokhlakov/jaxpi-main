cbd58dd08dda0948ec2278befde02367a80afed4
Khokhlakov <jmartinezcas@unal.edu.co>
2026-04-08 12:27:31 -0400

    syntax err

--- examples/l96_n40_f2/main.py
+++ examples/l96_n40_f2/main.py
@@ -36,7 +36,7 @@ def main(argv):
     elif FLAGS.config.mode == "eval":
         eval.evaluate(FLAGS.config, FLAGS.workdir)
 
-    elif FLAGS.mode == "eval_ekf":
+    elif FLAGS.config.mode == "eval_ekf":
         eval.evaluate_with_ekf(FLAGS.config, FLAGS.workdir)
 
 if __name__ == "__main__":
