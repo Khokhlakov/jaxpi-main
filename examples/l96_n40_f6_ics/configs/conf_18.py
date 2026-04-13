d270f89f124d3c010111da0ab4e02be84b34b381
Khokhlakov <jmartinezcas@unal.edu.co>
2026-04-13 14:41:00 -0400

    Added config 18

---
+++ examples/l96_n40_f6_ics/configs/conf_18.py
@@ -0,0 +1,87 @@
+import ml_collections
+import jax.numpy as jnp
+
+def get_config():
+    config = ml_collections.ConfigDict()
+    config.mode = "train"
+
+    # Weights & Biases
+    # Pwered config 15
+    config.wandb = wandb = ml_collections.ConfigDict()
+    wandb.project = "PI-UDON-L96-n40-f6-ics"
+    wandb.name = "test_18" 
+    wandb.tag = None
+
+    # Arch 
+    config.arch = arch = ml_collections.ConfigDict()
+    arch.arch_name = "DeepONet"
+    arch.num_branch_layers = 5
+    arch.num_trunk_layers = 5
+    arch.hidden_dim = 512
+    arch.branch_input_dim = 40
+    arch.out_dim = 40
+    arch.activation = "tanh"
+    arch.periodicity = None
+    arch.fourier_emb = ml_collections.ConfigDict({"embed_scale": 10, "embed_dim": 256})
+    arch.reparam = ml_collections.ConfigDict(
+        {"type": "weight_fact", "mean": 0.5, "stddev": 0.1}
+    )
+
+    # Optim
+    config.optim = optim = ml_collections.ConfigDict()
+    optim.grad_accum_steps = 0
+    optim.optimizer = "Soap"
+    optim.beta1 = 0.9
+    optim.beta2 = 0.999
+    optim.eps = 1e-8
+    optim.learning_rate = 1e-4
+    optim.decay_rate = 0.9
+    optim.decay_steps = 10_000 
+
+    # Training (Windowed Logic)
+    config.training = training = ml_collections.ConfigDict()
+    training.max_steps = 200000
+    training.batch_size_per_device = 512#16384
+    training.num_time_windows = 40
+    training.use_cartesian_prod = True
+    training.update_interval = 10000
+    training.num_initial_ics = 10000
+    training.max_additions = 0
+
+    # Weighting
+    config.weighting = weighting = ml_collections.ConfigDict()
+    weighting.scheme = "none"
+    weighting.init_weights = ml_collections.ConfigDict({"ics": 94.41337, "res": 0.50267}) 
+    weighting.momentum = 0.9
+    weighting.update_every_steps = 500
+
+    # Causal Weighting
+    weighting.use_causal = False
+    weighting.causal_tol = 0.02
+    weighting.num_chunks = 8
+
+    # Logging
+    config.logging = logging = ml_collections.ConfigDict()
+    logging.log_every_steps = 500
+    logging.log_errors = True
+    logging.log_losses = True
+    logging.log_weights = True
+    logging.log_preds = False
+    logging.log_grads = False
+    logging.log_ntk = False
+
+    # Saving
+    config.saving = saving = ml_collections.ConfigDict()
+    saving.save_every_steps = 10000
+    saving.num_keep_ckpts = 3
+    saving.restore_checkpoint = True
+    saving.restore_checkpoint_path = "test_18/ckpt/udon_model"
+    saving.total_plots = 5
+
+    # Input shape (t is the only input)
+    config.input_dim = 41
+
+    # Integer for PRNG random seed.
+    config.seed = 42
+
+    return config
