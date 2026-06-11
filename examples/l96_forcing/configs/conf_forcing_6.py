c7e2f382b16da40de88de79150e20844eed80f8f
Khokhlakov <jmartinezcas@unal.edu.co>
2026-06-11 03:54:12 -0400

    renamed config 6

--- examples/l96_forcing/configs/conf_forcing_6.py
+++ examples/l96_forcing/configs/conf_forcing_6.py
@@ -10,8 +10,8 @@ def get_config():
     # rerun of conf 2 8 with the modified l2 computation
     config.wandb = wandb = ml_collections.ConfigDict()
     wandb.project       = "PI-UDON-L96-F"
-    wandb.name          = "test_forcing_5" 
-    wandb.ckpt_name     = "test_forcing_5" 
+    wandb.name          = "test_forcing_6" 
+    wandb.ckpt_name     = "test_forcing_6" 
     wandb.tag = None
 
     # Arch 
