a7b01b1d9130238269f7e3db7f9ce05223898c39
Khokhlakov <jmartinezcas@unal.edu.co>
2026-09-07 03:53:00 -0400

    reduced  data size for KS 025

--- examples/KS_w_025/data/gen_data.py
+++ examples/KS_w_025/data/gen_data.py
@@ -313,7 +313,7 @@ if __name__ == "__main__":
                       N=256,
                       dt=0.005,
                       t_burn=100.0,
-                      max_additions=700,
+                      max_additions=1000,
                       test_windows=500,
                       batch_size=25)
 
