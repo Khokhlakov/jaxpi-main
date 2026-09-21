26f9dcb10b5a5543bacb02f838b8f3e0fc707f19
Khokhlakov <jmartinezcas@unal.edu.co>
2026-09-20 21:12:37 -0400

    TYPO in: added flexible pipeline for evaluation and plotting

--- examples/l96_f/configs/compare_base.py
+++ examples/l96_f/configs/compare_base.py
@@ -126,9 +126,9 @@ def get_config():
 
     # Evaluation
     config.eval = eval = ml_collections.ConfigDict()
-    eval.windows            = 500
+    eval.windows            = 320
     eval.trajectory_windows = 200
-    eval.num_ics            = 300
+    eval.num_ics            = 500
     eval.dt_integration     = 0.005
 
     eval.test_data_name = "l96_forcing_test"
